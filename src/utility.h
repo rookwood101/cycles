e1e514a0f6b40315d6db00a9806219ee84c352be
Alex Sadler <alexandersadler1@gmail.com>
2014-04-12 01:23:01 +0100

    Fix github bs

--- src/utility.h
+++ src/utility.h
@@ -8,18 +8,11 @@
 struct ProgramSettings {
 	bool exit;
 	int cache_distance_days;
-<<<<<<< HEAD
 	vector<string>::size_type max_tasks_other;
 	CalendarDatabase calendars;
 
 	ProgramSettings(): exit(false), cache_distance_days(30), max_tasks_other(10) {}
-=======
-	int max_tasks_other;
-	CalendarDatabase calendars;
-
-	ProgramSettings(): exit(false), cache_distance_days(30), max_tasks_other(10), {}
 };
->>>>>>> 49ac88bbd2a5f88b4a181641c490369a1bc13cee
 
 bool stringIsInteger(std::string input);
 
