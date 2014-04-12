ef11d34556f3b3892746e0da67363ee40ac594e0
Alex Sadler <alexandersadler1@gmail.com>
2014-04-12 01:24:35 +0100

    missed out namespace

--- src/utility.h
+++ src/utility.h
@@ -8,7 +8,7 @@
 struct ProgramSettings {
 	bool exit;
 	int cache_distance_days;
-	vector<string>::size_type max_tasks_other;
+	std::vector<std::string>::size_type max_tasks_other;
 	CalendarDatabase calendars;
 
 	ProgramSettings(): exit(false), cache_distance_days(30), max_tasks_other(10) {}
